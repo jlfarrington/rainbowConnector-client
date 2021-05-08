506800844240f0926bac0969b7aeac61a069a35c
Jenn Farrington <jlfarrington0@gmail.com>
2021-05-08 08:32:44 -0400

    adjust env file

--- src/helpers/environment.ts
+++ src/helpers/environment.ts
@@ -5,7 +5,7 @@ switch(window.location.hostname) {
     case '127.0.0.1':
         APIURL = 'http://localhost:3000'
         break
-    case 'https://rainbowconnector.herokuapp.com':
+    case 'http://rainbowconnector.herokuapp.com':
         APIURL = 'https://rainbow-connector.herokuapp.com'
 }
 
